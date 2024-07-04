fdeaf9958291256a2025000eb7d2bcb061959f4a
Vichtor <viktorfunny5000@gmail.com>
2024-07-04 17:14:45 -0300

    Aula de concatenação número e texto

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,26 @@
+public class Operadores {
+     public static void main(String[] args) {
+        String concatenação = "?";
+
+        concatenação = 1+1+1+"1";
+ 
+        System.out.println(concatenação);
+ 
+        concatenação = 1+"1"+1+1;
+ 
+        System.out.println(concatenação);
+ 
+        concatenação = 1+"1"+1+"1";
+ 
+        System.out.println(concatenação);
+ 
+        concatenação = "1"+1+1+1;
+ 
+        System.out.println(concatenação);
+ 
+        concatenação = "1"+(1+1+1);
+ 
+        System.out.println(concatenação);
+
+    }
+}
