5b94a98b2a453cfa73a9f48014926d35ef7ea9a8
Vichtor <viktorfunny5000@gmail.com>
2024-07-04 17:28:50 -0300

    Tipos_Variáveis e Operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -3,19 +3,29 @@ public class TiposVariaveis {
        
         String meuNome = "VICTOR AMARAL";
 
+        System.out.println(meuNome);
+
         double salarioMinimo = 2500;
+
+        System.out.println(salarioMinimo);
         
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
 
+        System.out.println(numeroNormal);
+
         int numero = 5;
+
+        System.out.println(numero);
 
         numero = 10;
 
         System.out.println(numero);
 
         final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
     }
 }     
     
